bf9b66ddf4177d6f266e270e1c2e868633142664
Dimon1989k <dima1989k@ukr.net>
2022-10-27 12:13:11 +0300

    finish

--- src/components/Friends/FriendList.jsx
+++ src/components/Friends/FriendList.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
 import css from './Friends.module.css';
+import FriendListItem from '../FriendListItem/FriendListItem';
 import PropTypes from 'prop-types';
 
- const FriendListItem = ({ friends }) => {
+ const FriendList = ({ friends }) => {
  
 
   return (
     <div className={css.friends}>
       <ul className={css.friends__list}>
         {friends.map(({ avatar, name, isOnline, id }) => (
-          <li className={css.friends__item} key={id}>
-            <span className={`${css.friends__status} ${css[isOnline]}`}>
-              {isOnline}
-            </span>
-            <img className={css.avatar} src={avatar} alt={name} />
-            <p className={css.friends__name}>{name}</p>
-          </li>
-        ))}
+           <FriendListItem
+          key={id}
+          name={name}
+          isOnline={isOnline}
+          avatar={avatar}
+        />
+      ))}    
       </ul>
     </div>
   );
 };
 
-FriendListItem.propTypes = {
+FriendList.propTypes = {
   FriendListItem: PropTypes.arrayOf(PropTypes.shape({
     ...FriendListItem.propTypes,
     id: PropTypes.number.isRequired,
@@ -30,4 +30,4 @@ FriendListItem.propTypes = {
 };
 
 
-export default FriendListItem;
+export default FriendList;
